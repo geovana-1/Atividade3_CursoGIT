9558cacd27ad61a529a7284c282b0b5fc8773750
Rubens Paes Neto <rubenspaesneto@gmail.com>
2026-05-21 15:07:03 -0300

    Feat: Criada a função de dividir

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function subtrair(a, b){
 function multiplicar(a, b){
     return a * b;
 }
+function dividir(a, b){
+    if (b === 0) {
+        return "Não é possível dividir por zero.";
+    } else {
+        return a / b;
+    }
+}
